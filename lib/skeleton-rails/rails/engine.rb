a0e5cba98269d6ea3f6df18ced2b9b1a78c1f011
Nelvin Driz <nelvindriz@live.com>
2011-12-22 23:36:11 +0800

    Unused Code - Causes DEPRECATION WARNING on rspec

--- lib/skeleton-rails/rails/engine.rb
+++ lib/skeleton-rails/rails/engine.rb
@@ -3,10 +3,6 @@ require 'rails'
 module SkeletonRails
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'skeleton-rails.setup', 
-        :group => :all do |app|
-          app.config.paths << File.join(config.root, 'vendor')
-      end
     end
   end
 end
